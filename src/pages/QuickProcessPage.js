5b328d9beba55eb158f37d9b09729ed5eec6af0c
Artem Dmitriev <art.dm.ser@gmail.com>
2016-11-05 11:17:52 +0300

    quick process improvements.

--- src/pages/QuickProcessPage.js
+++ src/pages/QuickProcessPage.js
@@ -18,8 +18,9 @@ class QuickProcessPage extends React.Component {
       errorStyle: {
           display: "none",
           value: "",
-          color: 'red'
+          className: 'callout-danger'
       },
+      isUploading: false,
       isProcessing: false
     };
   }
@@ -40,9 +41,10 @@ class QuickProcessPage extends React.Component {
           errorStyle: {
             display: {$set: "inline"},
             value: {$set: "Error: " + resp.message},
-            color: {$set: "red"}
+            className: {$set: 'callout-danger'}
           },
-          isProcessing: {$set: false}
+          isProcessing: {$set: false},
+          isUploading: {$set: false}
         }));
       }
     } else {
@@ -50,9 +52,10 @@ class QuickProcessPage extends React.Component {
         errorStyle: {
           display: {$set: "inline"},
           value: {$set: "Error: code=" + result.status + ", text='" + result.responseText + "'"},
-          color: {$set: "red"}
+          className: {$set: 'callout-danger'}
         },
-        isProcessing: {$set: false}
+        isProcessing: {$set: false},
+        isUploading: {$set: false}
       }));
     }
   }
@@ -63,18 +66,24 @@ class QuickProcessPage extends React.Component {
         errorStyle: {
           display: {$set: "inline"},
           value: {$set: "Processing GC logs, don't close this page ..."},
-          color: {$set: "black"}
+          className: {$set: 'callout-info'}
         },
-        isProcessing: {$set: true}
+        isProcessing: {$set: true},
+        isUploading: {$set: false}
       }));
     } else if (progress == 0) {
       this.setState(update(this.state, {
         errorStyle: {
             display: {$set: "none"},
             value: {$set: ""},
-            color: {$set: 'red'},
+            className: {$set: 'callout-danger'}
         },
-        isProcessing: {$set: false}
+        isProcessing: {$set: false},
+        isUploading: {$set: false}
+      }));
+    } else {
+      this.setState(update(this.state, {
+        isUploading: {$set: true}
       }));
     }
   }
@@ -84,9 +93,10 @@ class QuickProcessPage extends React.Component {
       errorStyle: {
         display: {$set: "inline"},
         value: {$set: "An error occured while uploading. Please refresh the page and try again."},
-        color: {$set: "red"},
+        className: {$set: 'callout-danger'}
       },
-      isProcessing: {$set: false}
+      isProcessing: {$set: false},
+      isUploading: {$set: false}
     }));
   }
 
@@ -95,10 +105,26 @@ class QuickProcessPage extends React.Component {
       errorStyle: {
         display: {$set: "inline"},
         value: {$set: "Upload aborted. Please refresh the page and try again."},
-        color: {$set: "red"},
+        className: {$set: 'callout-danger'}
       },
-      isProcessing: {$set: false}
+      isProcessing: {$set: false},
+      isUploading: {$set: false}
     }));
+  }
+
+  formGetter(){
+    return new FormData(document.getElementById('customForm'));
+  }
+
+  customFormRenderer(onSubmit){
+    return (
+      <form id='customForm' style={{marginBottom: '15px'}}>
+        <label htmlFor="exampleInputFile">File input</label>
+        <input style={{display: 'block'}} disabled={this.state.isUploading || this.state.isProcessing} type="file" name='file' id="exampleInputFile" />
+        <p/>
+        <Button type="button" bsStyle="primary" onClick={onSubmit}>Upload</Button>
+      </form>
+    );
   }
 
   render() {
@@ -111,19 +137,25 @@ class QuickProcessPage extends React.Component {
         </h1>
       </section>
       <section className="content">
-        <FileUploadProgress key='ex1' /*url='http://malsup.com/jquery/form/file-echo2.php'*/ url='http://gs-dev.gcplot.com/gc/jvm/log/process?token=d9ea6904f8ef0817eabd2774ce87b3786cf1828197e143dbaf749e928570a08b'
+        <FileUploadProgress key='ex1' url={GCPlotCore.authUrl("/gc/jvm/log/process")}
           onProgress={this.onProgress.bind(this)}
           onLoad={this.onLoad.bind(this)}
           onError={this.onError.bind(this)}
           onAbort={this.onAbort.bind(this)}
+          formGetter={this.formGetter.bind(this)}
+          formRenderer={this.customFormRenderer.bind(this)}
           />
           <p/>
+          <Col style={this.state.errorStyle} md={6}>
+          <div className={"callout " + this.state.errorStyle.className}>
           {(() => {
               if (this.state.isProcessing) {
                   return <Spinner spinnerName="three-bounce"/>
               }
           })()}
-          <p style={this.state.errorStyle}>{this.state.errorStyle.value}</p>
+            <p>{this.state.errorStyle.value}</p>
+          </div>
+          </Col>
       </section>
       </div>
     );
