4803150ddb7b8e32f136e86f0fddbdc78e990104
Artem Dmitriev <art.dm.ser@gmail.com>
2017-04-18 10:56:41 +0300

    fix.

--- src/layouts/WelcomeLayout.js
+++ src/layouts/WelcomeLayout.js
@@ -247,7 +247,8 @@ class WelcomeLayout extends React.Component {
                             </h3>
                             <h4><a href="https://docs.gcplot.com/log-files-processing.html" target="_blank">GCPlot Connector</a> technology allows you to deliver GC logs without any manual work, continuously</h4>
                             <h4>Check out our <a href="https://docs.gcplot.com" target="_blank">Knowledge Base</a> for more cool stuff</h4>
-                            <h3>
+                            <p />
+                            <h3 style="margin-top: 30px">
                                 Analyze graphs & stats
                             </h3>
                             <h4>Build a detailed report about any single collection</h4>
