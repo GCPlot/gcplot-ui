38154c36b625bc66bbaab43155644f8fd7784d21
Artem Dmitriev <art.dm.ser@gmail.com>
2017-04-20 09:14:24 +0300

    fix some text.

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -52,15 +52,15 @@ class DashboardPage extends React.Component {
     return <div className="content-wrapper">
     <section className="content-header">
       <h1>
-        
+
         <small>DASHBOARD</small>
       </h1>
     </section>
       <section className="content">
         <Panel>
           <p>We are currently at a <i>βeta</i> stage, so this page will be filling with something useful continuously.</p>
-          <p>You can already upload and process your Hotspot GC logs. There is no limitations on the amount of transmitted data at the moment, so do not miss this moment and go to <code><NavLink to="/quick_process">Quick Process</NavLink></code> page. We are also working hard on the <u>Realtime Connection</u> feature, which will allow you to continuously connect your JVM GC logs with our platform.</p>
-          <p>We would be glad to hear your questions and suggestions, as well as receive bug reports, at our <a href="https://groups.google.com/forum/#!forum/gcplot" target="_blank">Google Forum</a>. Alternatively, you can contact us directly at support@gcplot.com</p>
+          <p>You can already upload and process your Hotspot GC logs. There is no limitations on the amount of transmitted data at the moment, so do not miss this moment and go to <code><NavLink to="/quick_process">Quick Process</NavLink></code> page. We also have a <a href="https://docs.gcplot.com/log-files-processing.html" target="_blank">Realtime Connection</a> feature, which allows you to continuously connect your JVM GC logs with our platform.</p>
+          <p>We would be glad to hear your questions and suggestions, as well as receive bug reports, at our <a href="https://groups.google.com/forum/#!forum/gcplot" target="_blank">Google Forum</a>. Alternatively, you can contact us directly at <a href="mailto:support@gcplot.com">support@gcplot.com</a></p>
       </Panel>
       {(() => {
         if (!this.state.confirmed) {
